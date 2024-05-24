50ea89193b4aad2c7d22745a03f2371a93a01b8a
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-05-24 20:20:49 +0000

    contador

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import { func } from "prop-types";
+
+function SimpleCounter(){
+    return(<div className="bigCounter">
+        <div className="reloj"><i className="far fa-clock"></i>{}</div>
+        <div className="num1">0</div>
+        <div className="num2">0</div>
+        <div className="num3">0</div>
+        <div className="num4">0</div>
+        <div className="num5">0</div>
+        <div className="num6">0</div>
+    </div>)
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
